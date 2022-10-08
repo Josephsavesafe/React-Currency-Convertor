1afe3ad21c9dafa097d5b3c48e7950da86b3993c
josephwindows <tanawut247@gmail.com>
2022-10-08 20:42:14 +0700

    v1

--- src/App.js
+++ src/App.js
@@ -6,21 +6,62 @@ import { useEffect, useState } from "react";
 function App() {
   const [allCurrency, setAllCurrency] = useState([]);
 
-  const url = `https://api.exchangerate-api.com/v4/latest/USD`;
+  const [fromCurrency, setFromCurrency] = useState("USD");
+  const [toCurrency, setToCurrency] = useState("THB");
+
+  const [amount, setAmount] = useState(1);
+  const [rate, setRate] = useState(0);
+
+  const [checkFormCurrency, setCheckFormCurrency] = useState(true);
+  let fromAmount, toAmount;
+
   useEffect(() => {
+    const url = `https://api.exchangerate-api.com/v4/latest/${fromCurrency}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setAllCurrency([...Object.keys(data.rates)]));
-  }, []);
+      .then((data) => {
+        setAllCurrency([...Object.keys(data.rates)]);
+        setRate(data.rates[toCurrency]);
+      });
+  }, [fromCurrency, toCurrency]);
+
+  const amountFromCurrency = (e) => {
+    setAmount(e.target.value);
+    setCheckFormCurrency(true);
+  };
+  const amountToCurrency = (e) => {
+    setAmount(e.target.value);
+    setCheckFormCurrency(false);
+  };
+
+  if (checkFormCurrency) {
+    fromAmount = amount;
+    toAmount = (amount * rate).toFixed(2);
+  } else {
+    toAmount = amount;
+    fromAmount = (amount / rate).toFixed(2);
+  }
 
   return (
     <div>
       <img src={money} alt="money" className="money-img" />
       <h1 style={{ color: "chocolate" }}>Currency Converter</h1>
       <div className="container">
-        <CurrencyComponent Allcurrency={allCurrency} />
+        <CurrencyComponent
+          Allcurrency={allCurrency}
+          baseCurrency={fromCurrency}
+          changeCurrency={(e) => setFromCurrency(e.target.value)}
+          amount={fromAmount}
+          onChangeAmount={amountFromCurrency}
+        />
         <div className="equal">||</div>
-        <CurrencyComponent Allcurrency={allCurrency} />
+        <CurrencyComponent
+          Allcurrency={allCurrency}
+          baseCurrency={toCurrency}
+          changeCurrency={(e) => setToCurrency(e.target.value)}
+          amount={toAmount}
+          onChangeAmount={amountToCurrency}
+        />
       </div>
     </div>
   );
--- src/Components/CurrencyComponent.js
+++ src/Components/CurrencyComponent.js
@@ -3,12 +3,16 @@ import "./CurrencyComponent.css";
 const CurrencyComponent = (props) => {
   return (
     <div className="currency">
-      <select>
+      <select value={props.baseCurrency} onChange={props.changeCurrency}>
         {props.Allcurrency.map((currency) => (
           <option key={currency}>{currency}</option>
         ))}
       </select>
-      <input type="number" />
+      <input
+        type="number"
+        value={props.amount}
+        onChange={props.onChangeAmount}
+      />
     </div>
   );
 };
